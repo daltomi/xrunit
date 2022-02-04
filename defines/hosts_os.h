d7b3da62de73fb86bc883606c7c38dbd4b8a47ee
Daniel T. Borelli <danieltborelli@gmail.com>
2022-02-04 19:00:54 -0300

    Add hosts_os.h header file

---
+++ defines/hosts_os.h
@@ -0,0 +1,7 @@
+// Definition of all distributions supported by the program so far.
+#pragma once
+#define HOST_OS_ARTIXLINUX	0x100 /* by default, it has no header file	*/
+#define HOST_OS_VOIDLINUX	0x101 /* voidlinux.h						*/
+#undef HOST_OS
+
+
--- defines/voidlinux.h
+++ defines/voidlinux.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "hosts_os.h"
+#define HOST_OS HOST_OS_VOIDLINUX
 #define SV_DIR "/etc/sv"
 #define SV_RUN_DIR "/var/service"
 #define SYS_LOG_DIR "/var/log/socklog/"
