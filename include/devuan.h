3f22701539871842a8640ce0858562d3fb7aa118
Daniel T. Borelli <danieltborelli@gmail.com>
2023-07-19 18:54:08 -0300

    Added support for Devuan

---
+++ include/devuan.h
@@ -0,0 +1,15 @@
+// By default it is compiled with this header file.
+#pragma once
+
+#define HOST_OS "Devuan"
+
+#define SV "/usr/bin/sv"
+
+/*  Note: The paths of the directories without
+          the separator ('/') at the end. */
+
+#define SV_DIR "/etc/sv"
+
+#define SV_RUN_DIR "/etc/runit/runsvdir/current"
+
+#define SYS_LOG_DIR "/var/log/"
