c0244d0e506e5bcac0d854f3698e98f218753fa7
Daniel T. Borelli <danieltborelli@gmail.com>
2023-07-24 00:45:30 -0300

    Added support for Antix Linux

---
+++ include/antix.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define HOST_OS "Antix"
+
+#define SV "/usr/bin/sv"
+
+/*  Note: The paths of the directories without
+          the separator ('/') at the end. */
+
+#define SV_DIR "/etc/sv"
+
+#define SV_RUN_DIR "/etc/service"
+
+#define SYS_LOG_DIR "/var/log/runit"
